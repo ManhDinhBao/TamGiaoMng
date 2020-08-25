1a1faff9ad0527adfcacb622957b95ee5cd80bcc
ManhDB <ManhDB@DESKTOP-O7DV288>
2020-08-26 00:25:16 +0700

    Complete design Product Material Supplier

---
+++ TamGiaoMng/View/Material/ucMaterial.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamGiaoMng.View.Material
+{
+    /// <summary>
+    /// Interaction logic for ucMaterial.xaml
+    /// </summary>
+    public partial class ucMaterial : UserControl
+    {
+        public ucMaterial()
+        {
+            InitializeComponent();
+        }
+    }
+}
